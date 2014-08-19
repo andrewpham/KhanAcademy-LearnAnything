812dcd62a1190b6fcea0211e125d2579d1eff384
Andrew Tucker Pham <drew.t.pham@gmail.com>
2014-08-18 23:40:01 -0700

    Set up some boilerplate

--- app/src/main/java/com/andrewpham/android/khanacademy_learnanything/HomeActivity.java
+++ app/src/main/java/com/andrewpham/android/khanacademy_learnanything/HomeActivity.java
@@ -6,12 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 
 
