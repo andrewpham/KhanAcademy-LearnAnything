be9d02cef8e105f193003e15745a56af5b3e7b43
Andrew Pham <andrew.pham@udacity.com>
2014-09-05 14:58:51 -0700

    Set up Retrofit requests for each subject fragment

--- app/src/main/java/com/andrewpham/android/khanacademy_learnanything/TopicFragment.java
+++ app/src/main/java/com/andrewpham/android/khanacademy_learnanything/TopicFragment.java
@@ -1,18 +1,34 @@
 package com.andrewpham.android.khanacademy_learnanything;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.andrewpham.android.khanacademy_learnanything.api.ApiClient;
+import com.andrewpham.android.khanacademy_learnanything.topic_model.Child;
+import com.andrewpham.android.khanacademy_learnanything.topic_model.TopicData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by andrewpham on 8/26/14.
  */
 public class TopicFragment extends Fragment {
+
+    public static final String TAG = "TopicFragment";
+
     private static final String NODE_SLUG_TAG = "NodeSlugId";
     private String mNodeSlug;
+    private String[] mNodeSlugs;
 
     public static final TopicFragment newInstance(String nodeSlug) {
         TopicFragment fragment = new TopicFragment();
@@ -36,9 +52,59 @@ public class TopicFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_topic, container, false);
-        TextView textView = (TextView) v.findViewById(R.id.textView);
-        textView.setText(mNodeSlug);
+        new FetchItemsTask().execute();
 
         return v;
     }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            final ArrayList<String> nodeSlugs = new ArrayList<String>();
+            final ArrayList<String> titles = new ArrayList<String>();
+            final ArrayList<String> descriptions = new ArrayList<String>();
+            ApiClient.get().getTopicData(mNodeSlug, new Callback<TopicData>() {
+                @Override
+                public void success(TopicData topicData, Response response) {
+                    for (Child child : topicData.getChildren()) {
+                        String nodeSlug = child.getNodeSlug();
+                        nodeSlugs.add(nodeSlug);
+                        Log.d(TAG, nodeSlug);
+                    }
+                    mNodeSlugs = nodeSlugs.toArray(new String[nodeSlugs.size()]);
+                    Log.d(TAG, Arrays.toString(mNodeSlugs));
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+
+                }
+            });
+            return null;
+        }
+    }
+
+//
+//    private class GalleryItemAdapter extends ArrayAdapter<String> {
+//        public GalleryItemAdapter(ArrayList<String> items) {
+//            super(getActivity(), 0, items);
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            if (convertView == null) {
+//                convertView = getActivity().getLayoutInflater()
+//                        .inflate(R.layout.fragment_topic, parent, false);
+//            }
+//
+//            String item = getItem(position);
+//            TextView textView = (TextView) convertView.findViewById(R.id.title);
+//            TextView textView2 = (TextView) convertView.findViewById(R.id.description);
+//            textView.setText(titles.get(item));
+//            textView2.setText(descriptions.get(item));
+//
+//            return convertView;
+//        }
+//    }
+
 }
